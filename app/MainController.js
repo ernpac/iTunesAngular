a7cf6c4cd39d31f5921bb36ab2d3b7f830be939a
ernpac <ernpac@gmail.com>
2014-09-03 08:45:33 -0400

    initial commit

---
+++ app/MainController.js
@@ -0,0 +1,37 @@
+﻿var MainController = function (dataService) {
+    var _this = this;
+    _this.showError = false;
+    _this.hideTracks = false;
+    _this.model = [];
+    //_this.model.artistData = { 'test': 123 };
+    
+
+    _this.search = function () {
+        
+        var searchTerm = _this.model.searchTerm;
+        if (searchTerm) {
+            searchTerm = searchTerm.replace(' ', '+');
+            _this.showError = false;
+        } else {
+           
+            _this.error = "Please enter a search term.";
+            _this.showError = true;
+            return false;
+        }
+        dataService.search(searchTerm).
+        then(function (result) {
+            _this.model.artistData = result.data.results;
+            _this.showError = false;
+        }, function (error, status) {
+            _this.error = error.data.Message;
+            _this.showError = true;
+        });
+
+    }
+
+    _this.showRow = function (album) {
+        //alert(album.trackCount);
+        album.show = !album.show;
+    }
+}
+app.controller('MainController', MainController);
---
+++ app/app.js
@@ -0,0 +1,62 @@
+﻿var app = angular.module('app', ['kendo.directives', 'ngRoute'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', { templateUrl: '/app/views/main.html', controller: 'MainController', controllerAs: 'ctrl' })
+    .otherwise({ redirectTo: '/' })
+}]);
+
+app.factory('dataService', function ($http) {
+    function _search(searchTerm) {
+        var itunes = 'https://itunes.apple.com/search?term=ARTIST&entity=album';
+
+        itunes = itunes.replace('ARTIST', searchTerm);
+        return $http.jsonp(itunes, {
+            params: {
+                "callback": "JSON_CALLBACK",
+                "term": searchTerm
+            }
+        });
+    }
+
+    function _getTracks(id) {
+        var itunes = 'https://itunes.apple.com/lookup?id=' + id + '&entity=song';
+        return $http.jsonp(itunes, {
+            params: {
+                "callback": "JSON_CALLBACK",
+                "collectionId": id
+            }
+        });
+    }
+
+    return {
+        search: _search,
+        getTracks: _getTracks
+    }
+});
+
+app.directive('tracksColumn', ['dataService', function (dataService, $compile) {
+    var getTracks = function (scope, elem, attrs) {
+            var builder = '';
+            dataService.getTracks(scope.album)
+            .then(function (tracks) {
+                builder += '<ol>';
+                angular.forEach(tracks.data.results, function (track) {
+                    if (track.kind == 'song') {
+                        builder = builder + '<li>' + track.trackName + '</li>';
+                    }
+                })
+                elem.append(builder);
+                //$compile(builder)(scope);
+            });
+    }
+
+    return {
+        restrict: 'AE',
+        replace: false,
+        template: '',
+        scope: {
+            album: '@album'
+        },
+        link: getTracks
+    }
+}]);
